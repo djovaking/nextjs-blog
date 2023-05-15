3e1188000b2482cea4204fa7949cba29eab7efd4
Yolo <djovaking@gmail.com>
2023-05-15 18:59:11 +0200

    tuto jusqu'à l'implementation du css

---
+++ pages/posts/fraise/test.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function fraise() {
+  return (
+    <>
+      <h1>Premier Post!</h1>
+      <h2>
+        <Link href="/"> page d'accueil</Link>
+      </h2>
+    </>
+  );
+}
